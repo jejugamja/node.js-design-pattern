549fab0dc962b86aec9620abb0dce76da7f4c0ce
Jung Changyoung <surosoro@gmail.com>
2021-08-09 01:12:49 +0900

    change

--- chapter_07/7.2.1/lib/db.js
+++ chapter_07/7.2.1/lib/db.js
@@ -1,6 +1,8 @@
 const level = require('level')
 const sublevel = require('level-sublevel')
 
+// statefull 한 상태이다.
+// sublevel로 instance를 만들어서 exports
 module.exports = sublevel(
   level('example-db', { valueEncoding: 'json' })
 )
